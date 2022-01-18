8144e22489205bb0b8dd7c7da7e6c821d4caa733
yair-hb <aythb@hotmail.com>
2022-01-17 20:24:37 -0600

    se completa el codigo para Blurry face en video streaming

---
+++ Blurry_Face_VideoStreaming.py
@@ -0,0 +1,32 @@
+import cv2
+
+def algo (x):
+    pass
+
+captura = cv2.VideoCapture(0)
+faceclassif = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cv2.namedWindow('frame')
+cv2.createTrackbar('Blur','frame',0,15,algo)
+cv2.createTrackbar('Gray','frame',0,1,algo)
+
+while True:
+    ret, frame = captura.read()
+    valor = cv2.getTrackbarPos('Blur', 'frame')
+    valorGris = cv2.getTrackbarPos('Gray','frame')
+    if valorGris ==1:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    rostros = faceclassif.detectMultiScale(frame,1.3,5)
+
+    for (x,y,w,h) in rostros:
+        if valor > 0:
+            frame[y:y+h,x:x+w] = cv2.blur(frame[y:y+h,x:x+w],(valor,valor))
+    
+    cv2.imshow('frame', frame)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+captura.release()
+cv2.destroyAllWindows()
+
+
